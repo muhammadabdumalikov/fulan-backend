fa03e993d2cc45b59dedea1a3950071b6ab370ba
muhammadabdumalikov <burakali424@gmail.com>
2022-01-27 20:01:39 +0500

    add routes

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-import Express from "express"
-import DotEnv from "dotenv"
+import Express from "express";
+import DotEnv from "dotenv";
+import { ErrorHandler } from "./src/helpers/errorHandler";
 
-const PORT = process.env.PORT || 4443
+const PORT = process.env.PORT || 4443;
 
-async function server(){
-
+async function server() {
     //Create server
-    const app = Express()
+    const app = Express();
 
     //Run server
-    app.listen(PORT, () => console.log(`Server ready at${PORT}`))
+    app.listen(PORT, () => console.log(`Server ready at${PORT}`));
 
     //Middlewares
-    app.use(cors({origin: "*"}))
-    app.use(Express.urlencoded({extended: true}))
-    app.use(Express.json())
+    app.use(cors({ origin: "*" }));
+    app.use(Express.urlencoded({ extended: true }));
+    app.use(Express.json());
 
+    //ErrorHandler helper
+    app.use((req, res, next) => {
+        res.err = ErrorHandler;
+        next();
+    });
 }
