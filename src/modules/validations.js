8b1c1016ca96d9b74eb016cc586a7eae747a9bbe
muhammadabdumalikov <burakali424@gmail.com>
2022-01-31 21:40:37 +0500

    change login validations

--- src/modules/validations.js
+++ src/modules/validations.js
@@ -3,6 +3,34 @@ import Joi from "joi";
 export class Validations {
     static async UserCreateAccountValidation() {
         return Joi.object({
+            user_phone: Joi.string()
+                .required()
+                .error(new Error("Phone number is incorrect"))
+                .pattern(/^998[389][012345789][0-9]{7}$/),
+        });
+    }
+
+    static async ValidateUserCodeValidation() {
+        return Joi.object({
+            validation_code: Joi.string()
+                .required()
+                .min(5)
+                .max(5)
+                .error(new Error("Invalid code")),
+        });
+    }
+
+    static async UserLoginAccount() {
+        return Joi.object({
+            user_phone: Joi.string()
+                .required()
+                .error(new Error("Phone number is incorrect"))
+                .pattern(/^998[389][012345789][0-9]{7}$/),
+        });
+    }
+
+    static async EditUserAccount() {
+        return Joi.object({
             first_name: Joi.string()
                 .required()
                 .error(
@@ -24,35 +52,16 @@ export class Validations {
             user_second_phone: Joi.string()
                 .error(new Error("Phone number is incorrect"))
                 .pattern(/^998[389][012345789][0-9]{7}$/),
-            // address: Joi.string().required().min(3).max(124),
-            // working: Joi.boolean().required(),
-            // birth_date: Joi.date()
-            //     .required()
-            //     .error(new Error("You must be greater than *"))
-            //     .timestamp()
-            //     .greater("now"),
-            // about_self: Joi.string(),
-            // summ: Joi.string().required(),
-            // definition: Joi.string().required(),
-        });
-    }
-
-    static async ValidateUserCodeValidation() {
-        return Joi.object({
-            validation_code: Joi.string()
+            address: Joi.string().required().min(3).max(124),
+            working: Joi.boolean().required(),
+            birth_date: Joi.date()
                 .required()
-                .min(5)
-                .max(5)
-                .error(new Error("Invalid code")),
-        });
-    }
-
-    static async UserLoginAccount() {
-        return Joi.object({
-            user_phone: Joi.string()
-                .required()
-                .error(new Error("Phone number is incorrect"))
-                .pattern(/^998[389][012345789][0-9]{7}$/),
+                .error(new Error("You must be greater than *"))
+                .timestamp()
+                .greater("now"),
+            about_self: Joi.string(),
+            summ: Joi.string().required(),
+            definition: Joi.string().required(),
         });
     }
 }
