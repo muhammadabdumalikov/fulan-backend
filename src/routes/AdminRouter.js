ce2fe7aff2080f105315be145179780cf9e907e4
muhammadabdumalikov <burakali424@gmail.com>
2022-02-01 11:28:24 +0500

    add

--- src/routes/AdminRouter.js
+++ src/routes/AdminRouter.js
@@ -1,16 +1,16 @@
-import Express from "express"
-import AdminController from "../controllers/AdminController.js"
+import Express from "express";
+import AdminController from "../controllers/AdminController.js";
+import AdminMiddleware from "../middlewares/AdminMiddleware.js";
 
-const AdminRouter = Express.Router()
+const AdminRouter = Express.Router();
 
-AdminRouter.post('/login', AdminController.AdminLoginAccount)
-AdminRouter.post('/code', AdminController.ValidateAdminCode)
-AdminRouter.post('/addadmin', AdminController.AddAdminControl)
+AdminRouter.post("/login", AdminMiddleware, AdminController.AdminLoginAccount);
+AdminRouter.post("/code", AdminMiddleware, AdminController.ValidateAdminCode);
+AdminRouter.post("/addadmin", AdminMiddleware, AdminController.AddAdminControl);
 
-AdminRouter.get("/users", AdminController.GetAllUsers)
-
+AdminRouter.get("/users", AdminMiddleware, AdminController.GetAllUsers);
 
 export default {
     path: "/api/admin",
-    router: AdminRouter
-}
+    router: AdminRouter,
+};
